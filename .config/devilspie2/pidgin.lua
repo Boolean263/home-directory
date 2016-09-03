62768afcd15664c2d82fa235382b2a2d5cb51679
David Perry <d.perry@utoronto.ca>
2016-09-03 12:14:23 -0400

    Tweak pidgin layout

--- .config/devilspie2/pidgin.lua
+++ .config/devilspie2/pidgin.lua
@@ -1,5 +1,5 @@
 if get_window_class() == 'Pidgin' and get_window_role() == 'conversation' then
     pin_window()
     stick_window()
-    set_window_geometry(1693, 0, 200, 1028)
+    set_window_geometry2(1720, 0, 200, 1060)
 end
