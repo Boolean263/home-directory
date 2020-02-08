2abbdc39c35debe4bd358f20d14bdf9cd7f5ae26
David Perry <d.perry@utoronto.ca>
2020-02-08 08:25:06 -0500

    Refine XCompiled with proper keysyms

--- .XCompile.d/regen.sh
+++ .XCompile.d/regen.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-cat ~/.XCompile.d/*.txt | ~/git/XCompile/XCompile > ~/.XCompiled
+#cat ~/.XCompile.d/*.txt | ~/git/XCompile/XCompile > ~/.XCompiled
+~/git/NXCompile/NXCompile -s -n -o ~/.XCompiled ~/.XCompile.d/*.txt
+
+fixkeys
