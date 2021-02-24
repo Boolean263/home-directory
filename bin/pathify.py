0424e3abfc0f371c872d541c5402fd3998d1cadf
David Perry <boolean263@protonmail.com>
2021-02-24 17:40:08 -0500

    Add pathify.py and use it for path manipulation

---
+++ bin/pathify.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import os
+
+from collections import OrderedDict
+from collections.abc import Mapping
+
+#
+# Globals
+#
+
+global separator
+
+#
+# Functions
+#
+
+
+def to_OD(aIn):
+    """
+    Convert a path string or a list into an ordered dict.
+    """
+    rv = OrderedDict()
+
+    if type(aIn) is str:
+        aIn = aIn.split(separator)
+
+    for item in aIn:
+        rv[item] = 1
+
+    return rv
+
+
+def outpath(pathvar, paths):
+    """
+    Print the new path variable in a manner suitable for parsing
+    by the shell.
+    """
+    if isinstance(paths, Mapping):
+        paths = paths.keys()
+    print("export {0}={1}".format(pathvar, separator.join(paths).replace(" ", "\\ ")))
+
+
+#
+# Main Program
+#
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="TBD")
+    parser.add_argument(
+        "-v", "--verbose", action="count", default=0, help="Increase verbosity"
+    )
+    parser.add_argument(
+        "-s",
+        "--separator",
+        metavar="SEP",
+        type=str,
+        default=":",
+        help="Separator to use between search paths (currently ignored)",
+    )
+    parser.add_argument(
+        "pathvar", metavar="PATHVAR", help="Path variable upon which to operate"
+    )
+    subparsers = parser.add_subparsers(
+        title="Actions",
+        dest="action",
+        required=True,
+    )
+    subparsers.add_parser("clean", help="Remove duplicate path entries")
+    subparsers.add_parser("show", help="Show the current paths as a list")
+
+    p_add = subparsers.add_parser("add", help="Add entries to the path list")
+    p_add.add_argument(
+        "-e",
+        dest="append",
+        action="store_true",
+        help="Add to end of pathvar (default: add to start)",
+    )
+    p_add.add_argument(
+        "-f",
+        dest="force",
+        action="store_true",
+        help="Force-add (remove if already existing)",
+    )
+    p_add.add_argument("paths", nargs="+", type=str, help="Paths to add")
+
+    p_del = subparsers.add_parser("del", help="Remove entries from path list")
+    p_del.add_argument("paths", nargs="+", type=str, help="Paths to delete")
+
+    args = parser.parse_args()
+    separator = args.separator
+    if os.getenv(args.pathvar) is None:
+        parser.error(
+            "{0} does not appear to be a valid path variable".format(args.pathvar)
+        )
+
+    paths = to_OD(os.getenv(args.pathvar))
+
+    if args.action == "clean":
+        outpath(args.pathvar, paths)
+
+    elif args.action == "show":
+        for item in os.getenv(args.pathvar).split(separator):
+            print(item)
+
+    elif args.action == "add":
+        new_paths = list(to_OD(args.paths).keys())
+        if args.force:
+            for item in new_paths:
+                paths.pop(item, None)
+        else:
+            new_paths = [x for x in new_paths if x not in paths]
+
+        if args.append:
+            for item in new_paths:
+                paths[item] = 1
+        else:
+            for item in paths.keys():
+                new_paths.append(item)
+            paths = new_paths
+
+        outpath(args.pathvar, paths)
+
+    elif args.action == "del":
+        for item in args.paths:
+            paths.pop(item, None)
+
+        outpath(args.pathvar, paths)
+
+    else:
+        parser.error("Unrecognized command '{0}'".format(args.action))
+
+
+# Editor modelines - http://www.wireshark.org/tools/modelines.html
+#
+# Local variables:
+# c-basic-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# coding: utf-8
+# End:
+#
+# vi:set shiftwidth=4 tabstop=4 expandtab fileencoding=utf-8:
+# :indentSize=4:tabSize=4:noTabs=true:coding=utf-8:
--- etc/env/path_functions.bash
+++ etc/env/path_functions.bash
@@ -8,137 +8,175 @@
 # and then source this file with something like
 #    source /dev/stdin < <(sed 's/^\(.*\)()$/function \1/' etc/env/path_functions.bash)
 
-### is_in_path
-# Returns true if the directory in TESTPATH is a member in the path
-# variable PATHVAR.
-is_in_path()
-{
-    local PATHVAR="$1"
-    local TESTPATH="$2"
-    local SEP=":"
-    eval echo \$$PATHVAR | grep -q '\(^\|'"$SEP"'\)'"$TESTPATH"'\('"$SEP"'\|$\)' 2>/dev/null
-    return $?
-}
+# Pure bash was getting slow, so I've got a helper script.
+if type pathify.py >/dev/null 2>&1 ; then
 
-### add_to_path
-# Add to the path variable named in $1 the paths listed after, in order.
-# Adds to the beginning by default, or to the end if -e is given before
-# the path variable name.
-# If -bDIR or -aDIR is given before the path variable name, adds the new
-# paths before or after DIR if it appears in the path.
-# Forces adding/moving the path to the requested location if -f is given
-# before the path variable name.
-add_to_path()
-{
-    local TO_END=
-    local FORCE=
-    local SEP=":"
-    local BEFORE=""
-    local AFTER=""
-    local SED_SEP=$'\x1F' # sed supports using UNIT SEPARATOR as a delimiter
-                          # which (probably) won't appear in path names
-    while true ; do
-        case "$1" in
-            -e) TO_END=1 ; shift ;;
-            -f) FORCE=1  ; shift ;;
-            -b*) BEFORE="${1#-b}" ; shift ;;
-            -a*) AFTER="${1#-a}" ; TO_END=1 ; shift ;;
-            *) break ;;
-        esac
-    done
-    local PATHVAR="$1"
-    if [ -d "$PATHVAR" ] ; then
-        echo "add_to_path: $PATHVAR appears to be a directory, not a path variable" 1>&2
-    fi
-    shift
-    local ADDPATHS=""
-    if [ "x$BEFORE" != "x" ] && [ "x$AFTER" != "x" ] ; then
-        echo "add_to_path: -b and -a cannot both be specified" 1>&2
-        return 1
-    fi
-    while [ "x$1" != "x" ] ; do
-        [ "$FORCE" = "1" ] && del_from_path "$PATHVAR" "$1"
-        if ! [ -d "$1" ] ; then
-            echo "add_to_path: $1 not added to $PATHVAR (not found)"
-        elif ! is_in_path "$PATHVAR" "$1" && \
-             ! is_in_path ADDPATHS "$1" ; then
-            ADDPATHS="$ADDPATHS${ADDPATHS:+$SEP}$1"
+    add_to_path()
+    {
+        local args=""
+        while true ; do
+            case "$1" in
+                -e|-f) args="$args $1" ; shift ;;
+                *) break ;;
+            esac
+        done
+        local PATHVAR="$1"
+        shift
+        $(pathify.py "$PATHVAR" add $args $*)
+    }
+
+    del_from_path()
+    {
+        local PATHVAR="$1"
+        shift
+        $(pathify.py "$PATHVAR" del $*)
+    }
+
+    show_path()
+    {
+        pathify.py "$1" show
+    }
+
+    clean_path()
+    {
+        $(pathify.py "$1" clean)
+    }
+
+else
+    # Helper script not available, fall back on pure bash
+
+    ### is_in_path
+    # Returns true if the directory in TESTPATH is a member in the path
+    # variable PATHVAR.
+    is_in_path()
+    {
+        local PATHVAR="$1"
+        local TESTPATH="$2"
+        local SEP=":"
+        eval echo \$$PATHVAR | grep -q '\(^\|'"$SEP"'\)'"$TESTPATH"'\('"$SEP"'\|$\)' 2>/dev/null
+        return $?
+    }
+
+    ### add_to_path
+    # Add to the path variable named in $1 the paths listed after, in order.
+    # Adds to the beginning by default, or to the end if -e is given before
+    # the path variable name.
+    # If -bDIR or -aDIR is given before the path variable name, adds the new
+    # paths before or after DIR if it appears in the path.
+    # Forces adding/moving the path to the requested location if -f is given
+    # before the path variable name.
+    add_to_path()
+    {
+        local TO_END=
+        local FORCE=
+        local SEP=":"
+        local BEFORE=""
+        local AFTER=""
+        local SED_SEP=$'\x1F' # sed supports using UNIT SEPARATOR as a delimiter
+                            # which (probably) won't appear in path names
+        while true ; do
+            case "$1" in
+                -e) TO_END=1 ; shift ;;
+                -f) FORCE=1  ; shift ;;
+                -b*) BEFORE="${1#-b}" ; shift ;;
+                -a*) AFTER="${1#-a}" ; TO_END=1 ; shift ;;
+                *) break ;;
+            esac
+        done
+        local PATHVAR="$1"
+        if [ -d "$PATHVAR" ] ; then
+            echo "add_to_path: $PATHVAR appears to be a directory, not a path variable" 1>&2
         fi
         shift
-    done
-    eval local tmppath="\$$PATHVAR"
-    if [ "x$ADDPATHS" = "x" ] ; then
-        : # do nothing
-    elif [ "x$tmppath" = "x" ] ; then
-        eval export \$PATHVAR=\"$ADDPATHS\"
-    elif ! [ "x$BEFORE" = "x" ] && is_in_path "$PATHVAR" "$BEFORE" ; then
-        local SEDPATH=$(eval echo "\$$PATHVAR" | /bin/sed -r "s$SED_SEP"'(^|'"$SEP"')('"$BEFORE"')('"$SEP"'|$)'"$SED_SEP"'\1'"$ADDPATHS$SEP"'\2\3'"$SED_SEP")
-        eval export \$PATHVAR="$SEDPATH"
-    elif ! [ "x$AFTER" = "x" ] && is_in_path "$PATHVAR" "$AFTER" ; then
-        local SEDPATH=$(eval echo "\$$PATHVAR" | /bin/sed -r "s$SED_SEP"'(^|'"$SEP"')('"$AFTER"')('"$SEP"'|$)'"$SED_SEP"'\1\2'"$SEP$ADDPATHS"'\3'"$SED_SEP")
-        eval export \$PATHVAR="$SEDPATH"
-    elif [ "x$TO_END" = "x" ] ; then
-        eval export \$PATHVAR=\"$ADDPATHS$SEP\$$PATHVAR\"
-    else
-        eval export \$PATHVAR=\"\$$PATHVAR$SEP$ADDPATHS\"
-    fi
-}
-
-### del_from_path
-# Remove from the path variable named in $1 the paths listed after.
-del_from_path()
-{
-    local PATHVAR="$1"
-    local SEP=":"
-    if [ -d "$PATHVAR" ] ; then
-        echo "del_from_path: $PATHVAR appears to be a directory, not a path variable" 1>&2
-    fi
-    eval local tmppath=\"\$$PATHVAR\" # """
-    shift
-    if [ "x$1" = "x" ] ; then
-        echo "del_from_path: no paths found: did you forget your PATHVAR?" 1>&2
-        return 1
-    fi
-    while [ "x$1" != "x" ] ; do
-        local DELPATH="$1"
-        shift
-        if is_in_path "$PATHVAR" "$DELPATH" ; then
-            if [ "x$tmppath" = "x$DELPATH" ] ; then
-                # in case it's the entirety of the path
-                tmppath=""
-            else
-                # in case it's at the start
-                tmppath="${tmppath#$DELPATH$SEP}"
-
-                # in case it's at the end
-                tmppath="${tmppath%$SEP$DELPATH}"
-
-                # in case it's in the middle somewhere
-                tmppath="${tmppath/$SEP$DELPATH$SEP/$SEP}"
+        local ADDPATHS=""
+        if [ "x$BEFORE" != "x" ] && [ "x$AFTER" != "x" ] ; then
+            echo "add_to_path: -b and -a cannot both be specified" 1>&2
+            return 1
+        fi
+        while [ "x$1" != "x" ] ; do
+            [ "$FORCE" = "1" ] && del_from_path "$PATHVAR" "$1"
+            if ! [ -d "$1" ] ; then
+                echo "add_to_path: $1 not added to $PATHVAR (not found)"
+            elif ! is_in_path "$PATHVAR" "$1" && \
+                ! is_in_path ADDPATHS "$1" ; then
+                ADDPATHS="$ADDPATHS${ADDPATHS:+$SEP}$1"
             fi
+            shift
+        done
+        eval local tmppath="\$$PATHVAR"
+        if [ "x$ADDPATHS" = "x" ] ; then
+            : # do nothing
+        elif [ "x$tmppath" = "x" ] ; then
+            eval export \$PATHVAR=\"$ADDPATHS\"
+        elif ! [ "x$BEFORE" = "x" ] && is_in_path "$PATHVAR" "$BEFORE" ; then
+            local SEDPATH=$(eval echo "\$$PATHVAR" | /bin/sed -r "s$SED_SEP"'(^|'"$SEP"')('"$BEFORE"')('"$SEP"'|$)'"$SED_SEP"'\1'"$ADDPATHS$SEP"'\2\3'"$SED_SEP")
+            eval export \$PATHVAR="$SEDPATH"
+        elif ! [ "x$AFTER" = "x" ] && is_in_path "$PATHVAR" "$AFTER" ; then
+            local SEDPATH=$(eval echo "\$$PATHVAR" | /bin/sed -r "s$SED_SEP"'(^|'"$SEP"')('"$AFTER"')('"$SEP"'|$)'"$SED_SEP"'\1\2'"$SEP$ADDPATHS"'\3'"$SED_SEP")
+            eval export \$PATHVAR="$SEDPATH"
+        elif [ "x$TO_END" = "x" ] ; then
+            eval export \$PATHVAR=\"$ADDPATHS$SEP\$$PATHVAR\"
+        else
+            eval export \$PATHVAR=\"\$$PATHVAR$SEP$ADDPATHS\"
         fi
-    done
-    export $PATHVAR="$tmppath"
-}
+    }
 
-### show_path
-# Show the requested path variable with newlines instead of colons
-# to make it easier to read.
-show_path()
-{
-    local PATHVAR="$1"
-    local SEP=":"
-    if [ "x$PATHVAR" = "x" ] ; then
-        echo "show_path: no PATHVAR specified" 1>&2
-        return 1
-    fi
-    eval echo '$'$PATHVAR | /usr/bin/tr "$SEP" '\n'
-}
+    ### del_from_path
+    # Remove from the path variable named in $1 the paths listed after.
+    del_from_path()
+    {
+        local PATHVAR="$1"
+        local SEP=":"
+        if [ -d "$PATHVAR" ] ; then
+            echo "del_from_path: $PATHVAR appears to be a directory, not a path variable" 1>&2
+        fi
+        eval local tmppath=\"\$$PATHVAR\" # """
+        shift
+        if [ "x$1" = "x" ] ; then
+            echo "del_from_path: no paths found: did you forget your PATHVAR?" 1>&2
+            return 1
+        fi
+        while [ "x$1" != "x" ] ; do
+            local DELPATH="$1"
+            shift
+            if is_in_path "$PATHVAR" "$DELPATH" ; then
+                if [ "x$tmppath" = "x$DELPATH" ] ; then
+                    # in case it's the entirety of the path
+                    tmppath=""
+                else
+                    # in case it's at the start
+                    tmppath="${tmppath#$DELPATH$SEP}"
 
-### clean_path
-# Tidy the path string by removing duplicates.
-clean_path()
-{
-    local PATHVAR="$1"
-    add_to_path -f "$PATHVAR" $(show_path "$PATHVAR")
-}
+                    # in case it's at the end
+                    tmppath="${tmppath%$SEP$DELPATH}"
+
+                    # in case it's in the middle somewhere
+                    tmppath="${tmppath/$SEP$DELPATH$SEP/$SEP}"
+                fi
+            fi
+        done
+        export $PATHVAR="$tmppath"
+    }
+
+    ### show_path
+    # Show the requested path variable with newlines instead of colons
+    # to make it easier to read.
+    show_path()
+    {
+        local PATHVAR="$1"
+        local SEP=":"
+        if [ "x$PATHVAR" = "x" ] ; then
+            echo "show_path: no PATHVAR specified" 1>&2
+            return 1
+        fi
+        eval echo '$'$PATHVAR | /usr/bin/tr "$SEP" '\n'
+    }
+
+    ### clean_path
+    # Tidy the path string by removing duplicates.
+    clean_path()
+    {
+        local PATHVAR="$1"
+        add_to_path -f "$PATHVAR" $(show_path "$PATHVAR")
+    }
+fi
