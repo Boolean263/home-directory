1f21e99fef453627e31a65418565e019df811f1e
David Perry <boolean263@protonmail.com>
2021-06-21 20:28:21 -0400

    Some wezterm tweaks

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,24 +1,46 @@
 local wz = require 'wezterm'
 
+local function home_file_exists(name)
+   local f=io.open(os.getenv('HOME').."/"..name,"r")
+   if f~=nil then
+       io.close(f)
+       return true
+   else
+       return false
+   end
+end
+
 local my_fonts = {
-    "Iosevka Term",
+    "Iosevka Fixed",
     "Source Code Pro",
     "monospace",
 }
 
+local myterm
+if home_file_exists(".terminfo/w/wezterm") then
+    myterm = "wezterm"
+else
+    -- This is the default
+    myterm = "xterm-256color"
+end
+
 return {
     enable_tab_bar = false,
     enable_wayland = true,
+    term = myterm,
 
+    -- https://wezfurlong.org/wezterm/config/fonts.html
     font_size = 12.0,
     font = wz.font_with_fallback(my_fonts),
     font_antialias = "Subpixel",
     font_hinting = "Full",
 
+    -- https://wezfurlong.org/wezterm/config/keys.html
     keys = {
         {key="Backspace", mods="CTRL", action=wz.action{SendString="\x08"}},
     },
 
+    -- https://wezfurlong.org/wezterm/config/appearance.html
     bold_brightens_ansi_colors = true,
     colors = {
         ansi = {
@@ -41,5 +63,9 @@ return {
             '#00ffff',
             '#ffffff',
         },
+    },
+
+    set_environment_variables = {
+        COLORTERM="truecolor",
     },
 }
