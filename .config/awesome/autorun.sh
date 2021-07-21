2553a375fb38c87b3e8bb1d27c2002956a520adb
David Perry <boolean263@protonmail.com>
2021-07-21 09:15:27 -0400

    Some tweaks to awesomewm

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 # Prevent from running more than once per X session
-if xrdb -query | grep -q '^awesome\.started:\s*true$'; then exit; fi
+if xrdb -query | grep -q '^awesome\.started:\s*true$'; then
+    exit
+fi
 echo 'awesome.started: true' | xrdb -merge -
 
 # Clone some extra repos here
 for repo in \
-    https://github.com/streetturtle/awesome-buttons \
-    git@github.com:streetturtle/awesome-wm-widgets.git \
-    git@github.com:xinhaoyuan/layout-machi.git \
+    https://github.com/streetturtle/awesome-buttons.git \
+    https://github.com/streetturtle/awesome-wm-widgets.git \
+    https://github.com/xinhaoyuan/layout-machi.git \
 ; do
     dirname=${repo%%.git}
     dirname=${dirname##*/}
--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -122,11 +122,11 @@ altkey = "Mod1"
 awful.layout.layouts = {
     awful.layout.suit.tile.left,
     -- awful.layout.suit.tile,
-    awful.layout.suit.floating,
+    -- awful.layout.suit.floating,
     -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
     -- awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
+    awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
     -- awful.layout.suit.max,
