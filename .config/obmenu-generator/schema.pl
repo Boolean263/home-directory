d8d9caf9609527ead37861c685d57e4958efd4eb
David Perry <boolean263@protonmail.com>
2023-08-29 08:58:21 -0400

    Use obmenu-generator to generate openbox menus

---
+++ .config/obmenu-generator/schema.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+# Origin: https://github.com/trizen/obmenu-generator
+# I've chopped this down from its stock values to only show applications,
+# without added options for editing config files and whatnot.
+
+=for comment
+
+    item:      add an item inside the menu               {item => ["command", "label", "icon"]},
+    cat:       add a category inside the menu             {cat => ["name", "label", "icon"]},
+    sep:       horizontal line separator                  {sep => undef}, {sep => "label"},
+    pipe:      a pipe menu entry                         {pipe => ["command", "label", "icon"]},
+    file:      include the content of an XML file        {file => "/path/to/file.xml"},
+    raw:       any XML data supported by Openbox          {raw => q(...)},
+    beg:       begin of a category                        {beg => ["name", "icon"]},
+    end:       end of a category                          {end => undef},
+    obgenmenu: generic menu settings                {obgenmenu => ["label", "icon"]},
+    exit:      default "Exit" action                     {exit => ["label", "icon"]},
+
+=cut
+
+# NOTE:
+#    * Keys and values are case sensitive. Keep all keys lowercase.
+#    * ICON can be a either a direct path to an icon or a valid icon name
+#    * Category names are case insensitive. (X-XFCE and x_xfce are equivalent)
+
+require "$ENV{HOME}/.config/obmenu-generator/config.pl";
+
+## Text editor
+my $editor = $CONFIG->{editor};
+
+our $SCHEMA = [
+
+    #          NAME            LABEL                ICON
+    {cat => ['utility',     'Accessories', 'applications-utilities']},
+    {cat => ['development', 'Development', 'applications-development']},
+    {cat => ['education',   'Education',   'applications-science']},
+    {cat => ['game',        'Games',       'applications-games']},
+    {cat => ['graphics',    'Graphics',    'applications-graphics']},
+    {cat => ['audiovideo',  'Multimedia',  'applications-multimedia']},
+    {cat => ['network',     'Network',     'applications-internet']},
+    {cat => ['office',      'Office',      'applications-office']},
+    {cat => ['other',       'Other',       'applications-other']},
+    {cat => ['settings',    'Settings',    'applications-accessories']},
+    {cat => ['system',      'System',      'applications-system']},
+
+]
