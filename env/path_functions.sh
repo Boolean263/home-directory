4c7961d8629f086326377596e5b26b3cdbc3abdf
David Perry <boolean263@protonmail.com>
2025-10-28 07:55:11 -0400

    path_functions.sh improvements: -sSEP, -n
    Allow passing `-sSEP` to give a different path separator from `:`;
    useful for e.g. Lua paths which use `;` as their separator.
    
    Add `add_to_path -n` to allow adding nonexistent directories to the
    path.
    
    Touch up some comments.

--- env/path_functions.sh
+++ env/path_functions.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 #
-# Shell (bash) functions for manipulating colon-separated paths.
+# Shell functions for manipulating colon-separated paths.
 # Mainly useful by other shell scripts, but sometimes handy manually too.
 #
-# bash 5.0 doesn't seem to like the older `function foo` syntax.
+# bash 5.0 doesn't seem to like the older `function foo` syntax that ksh needs.
 # To make this file work for ksh, run `alias local=typeset`
 # and then source this file with something like
-#    source /dev/stdin < <(sed 's/^\(.*\)()$/function \1/' etc/env/path_functions.bash)
+#    source /dev/stdin < <(sed 's/^\(.*\)()$/function \1/' /path/to/path_functions.sh)
 
 ### exists
 # Tell if a runnable program exists with the given name.
 exists()
 {
-    which "$1" >/dev/null 2>&1
+    # command is a shell builtin; which is an external binary
+    #which "$1" >/dev/null 2>&1
+    command -v "$1" >/dev/null 2>&1
 }
 
 ### is_in_path
 # Returns true if the directory in TESTPATH is a member in the path
-# variable PATHVAR.
+# variable PATHVAR. Meant to be a helper function.
 is_in_path()
 {
     local PATHVAR="$1"
     local TESTPATH="$2"
-    local SEP=":"
+    local SEP="${3:-:}"
     eval echo \$$PATHVAR \
         | grep -q '\(^\|'"$SEP"'\)'"$TESTPATH"'\('"$SEP"'\|$\)' \
         2>/dev/null
@@ -37,10 +39,14 @@ is_in_path()
 # paths before or after DIR if it appears in the path.
 # Forces adding/moving the path to the requested location if -f is given
 # before the path variable name.
+# Allows adding nonexistent paths if -n is given.
+# Set a different path separator from ":" with -sSEP.
 add_to_path()
 {
     local TO_END=
     local FORCE=
+    local QUIET=
+    local NONEXIST=
     local SEP=":"
     local IFS=""
     local BEFORE=""
@@ -50,10 +56,13 @@ add_to_path()
                                             # won't appear in path names
     while true ; do
         case "$1" in
-            -e) TO_END=1 ; shift ;;
-            -f) FORCE=1  ; shift ;;
-            -b*) BEFORE="${1#-b}" ; shift ;;
-            -a*) AFTER="${1#-a}" ; TO_END=1 ; shift ;;
+            -e) TO_END=1 ;                      shift ;;
+            -f) FORCE=1  ;                      shift ;;
+            -q) QUIET=1 ;                       shift ;;
+            -n) NONEXIST=1 ;                    shift ;;
+            -s*) SEP="${1#-s}" ;                shift ;;
+            -b*) BEFORE="${1#-b}" ;             shift ;;
+            -a*) AFTER="${1#-a}" ; TO_END=1 ;   shift ;;
             *) break ;;
         esac
     done
@@ -70,12 +79,19 @@ add_to_path()
         return 1
     fi
     while [ "x$1" != "x" ] ; do
-        [ "$FORCE" = "1" ] && del_from_path "$PATHVAR" "$1"
-        if ! [ -d "$1" ] ; then
-            echo "add_to_path: $1 not added to $PATHVAR (not found)"
-        elif ! is_in_path "$PATHVAR" "$1" && \
-            ! is_in_path ADDPATHS "$1" ; then
-                        ADDPATHS="$ADDPATHS${ADDPATHS:+$SEP}$1"
+        if [ "$FORCE" = "1" ] ; then
+            del_from_path "-s$SEP" "$PATHVAR" "$1"
+        fi
+        if ! is_in_path "$PATHVAR" "$1" "$SEP" && \
+                ! is_in_path ADD PATHS "$1" "$SEP" ; then
+            if ! [ -d "$1" ] && [ -z "$NONEXIST" ] ; then
+                if [ -z "$QUIET" ] ; then
+                    echo "add_to_path: $1 not added to $PATHVAR (not found)" 1>&2
+                fi
+                shift
+                continue
+            fi
+            ADDPATHS="$ADDPATHS${ADDPATHS:+$SEP}$1"
         fi
         shift
     done
@@ -84,11 +100,11 @@ add_to_path()
         : # do nothing
     elif [ "x$EXPANDED_PATH" = "x" ] ; then
         export $PATHVAR="$ADDPATHS"
-    elif ! [ "x$BEFORE" = "x" ] && is_in_path "$PATHVAR" "$BEFORE" ; then
+    elif ! [ "x$BEFORE" = "x" ] && is_in_path "$PATHVAR" "$BEFORE" "$SEP" ; then
         local SEDPATH=$(eval echo "\$$PATHVAR" \
             | /bin/sed -r "s$SED_SEP"'(^|'"$SEP"')('"$BEFORE"')('"$SEP"'|$)'"$SED_SEP"'\1'"$ADDPATHS$SEP"'\2\3'"$SED_SEP")
         export $PATHVAR="$SEDPATH"
-    elif ! [ "x$AFTER" = "x" ] && is_in_path "$PATHVAR" "$AFTER" ; then
+    elif ! [ "x$AFTER" = "x" ] && is_in_path "$PATHVAR" "$AFTER" "$SEP" ; then
         local SEDPATH=$(eval echo "\$$PATHVAR" \
             | /bin/sed -r "s$SED_SEP"'(^|'"$SEP"')('"$AFTER"')('"$SEP"'|$)'"$SED_SEP"'\1\2'"$SEP$ADDPATHS"'\3'"$SED_SEP")
         export $PATHVAR="$SEDPATH"
@@ -103,8 +119,14 @@ add_to_path()
 # Remove from the path variable named in $1 the paths listed after.
 del_from_path()
 {
-    local PATHVAR="$1"
     local SEP=":"
+    while true ; do
+        case "$1" in
+            -s*) SEP="${1#-s}" ; shift ;;
+            *) break ;;
+        esac
+    done
+    local PATHVAR="$1"
     if [ -d "$PATHVAR" ] ; then
         echo "del_from_path: " \
             "$PATHVAR appears to be a directory, not a path variable" 1>&2
@@ -131,8 +153,14 @@ del_from_path()
 # to make it easier to read.
 show_path()
 {
-    local PATHVAR="$1"
     local SEP=":"
+    while true ; do
+        case "$1" in
+            -s*) SEP="${1#-s}" ; shift ;;
+            *) break ;;
+        esac
+    done
+    local PATHVAR="$1"
     if [ "x$PATHVAR" = "x" ] ; then
         echo "show_path: no PATHVAR specified" 1>&2
         return 1
@@ -144,8 +172,14 @@ show_path()
 # Tidy the path string by removing duplicates.
 clean_path()
 {
-    local PATHVAR="$1"
     local SEP=":"
+    while true ; do
+        case "$1" in
+            -s*) SEP="${1#-s}" ; shift ;;
+            *) break ;;
+        esac
+    done
+    local PATHVAR="$1"
     local i
     local CONTENTS
 
@@ -153,6 +187,6 @@ clean_path()
     unset $PATHVAR
     local IFS="$SEP"
     for i in $CONTENTS; do
-        add_to_path -e "$PATHVAR" "$i"
+        add_to_path "-s$SEP" -e "$PATHVAR" "$i"
     done
 }
