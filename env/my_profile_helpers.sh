453d2a803d7b22e5526b35d36c760c7246a60828
David Perry <boolean263@protonmail.com>
2026-07-07 06:50:54 -0400

    profile: make source_all_in use bashisms
    My attempts to make a POSIX-universal version of the function kept
    failing, and I think I'll have bash everywhere I go.

--- env/my_profile_helpers.sh
+++ env/my_profile_helpers.sh
@@ -1,37 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 # shellcheck disable=SC3043 # don't warn on `local` since most shells support it
 #
 # Helper functions for use in my various shell profile configs:
 # .bashrc, .profile, etc.
-# Can't be bash-specific.
 
 ### source_all_in()
 # Source every file in the given directory matching the given find(1) criteria.
 # Example: source_all_in ~/.config/profile.d -name '*.sh'
-# Note: File read order doesn't seem to be guaranteed, despite using sort(1)
-# on the results.
+# Note: dependent on bash for `read -rd ''` and `< <(cmd)`,
+# and GNU sort(1) for its `-z` option
 source_all_in()
 {
     local FROM_DIR="${1:?no directory specified}"
     shift
     local CONF_FILE
+    #echo "source_all_in start $FROM_DIR $*" 1>&2
 
     if [ -d "$FROM_DIR" ] ; then
-        # Separating on nulls gives weird errors and doesn't work,
-        # so we use another delimiter instead. We save the current IFS
-        # so it doesn't affect the files we're sourcing.
-        local SaveIFS="$IFS"
-        local IFS
-        IFS="$(printf '\x1E')" # Record Separator; octal 036
-
         set -a
-        for CONF_FILE in $(find "$FROM_DIR" -maxdepth 1 \
-                -type f ${1:+"$@"} -printf '%h/%f\036' \
-            | LC_COLLATE=POSIX sort)
-        do
+        while read -rd '' CONF_FILE; do
+            #echo "source_all_in $CONF_FILE" 1>&2
             # shellcheck disable=SC1090 # sourcing non-constant file -- no kidding!
-            IFS="$SaveIFS" . "$CONF_FILE"
-        done
+            . "$CONF_FILE"
+        done < <(find "$FROM_DIR" -maxdepth 1 \
+                    -type f ${1:+"$@"} -print0 \
+                    | LC_COLLATE=POSIX sort -z)
         set +a
     fi
+    #echo "source_all_in end $FROM_DIR" 1>&2
 }
